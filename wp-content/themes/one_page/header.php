0ebc5e32081c23078ba963651f963f104abef066
Vojtěch Vašmucius <Vojtech.Vasmucius@student.souepl.cz>
2025-11-06 11:30:54 +0100

    6.11.

--- wp-content/themes/one_page/header.php
+++ wp-content/themes/one_page/header.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
-<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-
 <header>
   <div class="wrapper">
     <div class="wrapper-logo">
